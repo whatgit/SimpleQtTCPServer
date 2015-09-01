a5f2a49aa753f0c7968feb0cd16cff3f605466aa
Maytheewat Aramrattana <maytheewat.aramrattana@hh.se>
2015-09-01 13:59:31 +0200

    now buffer size depends on bytesAvailable()

--- tcpserver.cpp
+++ tcpserver.cpp
@@ -19,15 +19,23 @@ TCPServer::~TCPServer()
 void TCPServer::acceptConnection()
 {
   client = server.nextPendingConnection();
-
+  cout << "someone has connected" << endl;
   connect(client, SIGNAL(readyRead()),
     this, SLOT(startRead()));
 }
 
 void TCPServer::startRead()
 {
-  char buffer[1024] = {0};
+  if(client->state() == QTcpSocket::ConnectedState)
+  {
+  char* buffer = new char[client->bytesAvailable()];
+  //char buffer[1024];
   client->read(buffer, client->bytesAvailable());
-  cout << buffer << endl;
+  cout << "I received:" << buffer << endl;
+  //cout << buffer << endl;
+  client->write(buffer);
+  client->flush();
   //client->close();
+  delete[] buffer;
+  }
 }
